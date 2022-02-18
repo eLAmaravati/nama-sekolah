4130b073116366594efc76f6968106852c2d0f34
eLAmaravati <langit.amaravati@gmail.com>
2022-02-18 10:50:27 +0700

    draft3

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# nama-sekolah
-Daftar nama sekolah di Indonesia
+# Sakooolaaa - Praktik Membuat Web dari Public API
+
+Web ini dibuat dalam rangka praktik
+
+## Sumber
+
+- [Tutorial]
+- [API Daftar Nama Sekolah]
+
+## Dibuat dengan:
+
+- jQuery
+- Ajax
+- Bootstrap (for the sake of gak usah ngoding CSS)
--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
-$.getJSON('https://api-sekolah-indonesia.herokuapp.com/sekolah?page=1&perPage=7', function (data) {
+function semuaSekolah() {
+	$.getJSON('https://api-sekolah-indonesia.herokuapp.com/sekolah?page=1&perPage=100', function (data) {
 	
-	let daftarSekolah = data.dataSekolah;
+		let daftarSekolah = data.dataSekolah;
+	
+		$.each(daftarSekolah, function (i, data) {
+			$('#nama-sekolah').append(`
+			
+			<tr>
+					<th scope="row" class="count"></th>
+					<td>`+ data.bentuk +`</td>
+					<td>`+ data.sekolah +`</td>
+					<td>`+ data.npsn +`</td>
+					<td>`+ data.propinsi +`</td>
+					<td>`+ data.kabupaten_kota +`</td>
+			</tr>
+			`)
+			
+		});
+	});	
+}
 
-	$.each(daftarSekolah, function (i, data) {
-		$('#nama-sekolah').append(`
-		
-		<tr>
-				<th scope="row" class="count"></th>
-				<td>`+ data.bentuk +`</td>
-				<td>`+ data.sekolah +`</td>
-				<td>`+ data.npsn +`</td>
-				<td>`+ data.propinsi +`</td>
-				<td>`+ data.kabupaten_kota +`</td>
-		</tr>
-		`)
-		
-	});
+semuaSekolah();
+
+
+// Navbar Brand
+$('.navbar-brand').on('click', function () {
+	$('.nav-link').removeClass('active');
+	$(this).addClass('active');
+	semuaSekolah();
 });
 
 
@@ -28,7 +40,29 @@ $('.nav-link').on('click', function () {
 	let navigasi = $(this).html();
 	$('#jenjang-navigasi').html(navigasi);
 
+	
+	// Mengganti konten sesuai menu
+	$.getJSON('https://api-sekolah-indonesia.herokuapp.com/sekolah?page=1&perPage=50', function (data) {
+		
+		let daftarSekolah = data.dataSekolah;
+		let jenjangSekolah = '';
 
+		$.each(daftarSekolah, function (i, data) {
+			if (data.bentuk == navigasi) {
+				jenjangSekolah += `
+				<tr>
+				<th scope="row" class="count"></th>
+				<td>`+ data.bentuk + `</td>
+				<td>`+ data.sekolah + `</td>
+				<td>`+ data.npsn + `</td>
+				<td>`+ data.propinsi + `</td>
+				<td>`+ data.kabupaten_kota + `</td>
+		</tr>`;
+			}
+		});
+
+		$('#nama-sekolah').html(jenjangSekolah);
+	});
 	
 	
 });
